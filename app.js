d639fc13dc5a6dc69504d25a829eda5262e0fa61
Suraj Singh Bisht <singhbisht967@gmail.com>
2024-07-10 12:36:21 +0530

    Add Data in databse and make new files for the initilization of project

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const Listing = require("./models/listing.js");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
@@ -11,12 +12,25 @@ main().then(()=>{
 });
 
 async function main() {
-    mongoose.connect(MONGO_URL);
+    await mongoose.connect(MONGO_URL);
 }
 
 app.get("/", (req,res)=>{
     res.send("Hii, I'm root!");
-})
+});
+
+app.get("/testListing", async (req,res)=>{
+    let sampleListing = new Listing({
+        title: "My new Villa",
+        description:"By the beach",
+        price: 1200,
+        location: "Haldwani, Uttarakhand",
+        Country: "India",
+    });
+    await sampleListing.save();
+    console.log("Sample was saved");
+    res.send("Successful testing");
+});
 
 app.listen(8080, () => {
     console.log("Server is running on port 8080");
---
+++ models/listing.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const listingSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: String,
+  image: {
+    type: String,
+    default:
+      "https://archive.org/download/sea-beach-coconut-free-nature-bollywood_126323/sea-beach-coconut-free-nature-bollywood_126323.jpg",
+
+    set: (v) =>
+      v === ""
+        ? "https://archive.org/download/sea-beach-coconut-free-nature-bollywood_126323/sea-beach-coconut-free-nature-bollywood_126323.jpg"
+        : v,
+  },
+  price: Number,
+  location: String,
+  country: String,
+});
+
+const Listing = mongoose.model("Listing", listingSchema);
+module.exports = Listing;
